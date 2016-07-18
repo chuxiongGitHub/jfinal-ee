c81135b2467facb6805f4bb5e2bde3bd2bfd370a
chencaihong <836908728@qq.com>
2016-07-18 17:22:49 +0800

    jfinal插件配置：使用阿里druid数据库连接池

--- src/main/java/com/rainbow/Config.java
+++ src/main/java/com/rainbow/Config.java
@@ -2,6 +2,10 @@ package com.rainbow;
 
 import com.jfinal.config.*;
 import com.jfinal.core.JFinal;
+import com.jfinal.kit.PropKit;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.druid.DruidPlugin;
+import com.jfinal.render.ViewType;
 import com.rainbow.controller.HelloController;
 import com.rainbow.controller.TestController;
 
@@ -12,6 +16,8 @@ import com.rainbow.controller.TestController;
 public class Config extends JFinalConfig {
     @Override
     public void configConstant(Constants me) {
+        me.setEncoding("UTF-8");
+        me.setViewType(ViewType.JSP);
         me.setDevMode(true);
     }
 
@@ -23,6 +29,23 @@ public class Config extends JFinalConfig {
 
     @Override
     public void configPlugin(Plugins me) {
+//启用jfinal插件
+        PropKit.use("jdbc.properties");
+final String URL=PropKit.get("jdbcUrl");
+        final  String USERNAME=PropKit.get("jdbcUser");
+        final String PASSWORD=PropKit.get("jdbcPassword");
+        final String DRIVER=PropKit.get("jdbcDriver");
+        final Integer INITIALSIZE = PropKit.getInt("jdbcInitialSize");
+        final Integer MIDIDLE = PropKit.getInt("jdbcMaxIdle");
+        final Integer MAXACTIVEE = PropKit.getInt("jdbcMaxActive");
+        DruidPlugin druidPlugin=new DruidPlugin(URL,USERNAME,PASSWORD);
+        druidPlugin.set(INITIALSIZE,MIDIDLE,MAXACTIVEE);
+        druidPlugin.setFilters("stat,wall");
+        me.add(druidPlugin);
+
+        ActiveRecordPlugin activeRecordPlugin=new ActiveRecordPlugin(druidPlugin);
+        //加入数据表的映射
+        me.add(activeRecordPlugin);
 
     }
 
